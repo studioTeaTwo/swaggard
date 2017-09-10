371bd66bb0c65628a5e891acf54a57687a4d0c10
T2 <t2.tide@gmail.com>
2017-09-11 00:57:20 +0900

    Add new tag '@name' as alias name

--- lib/swaggard/parsers/controllers.rb
+++ lib/swaggard/parsers/controllers.rb
@@ -8,11 +8,18 @@ module Swaggard
       def run(yard_objects, routes)
         tag = nil
         operations = []
+        alias_names = []
 
         yard_objects.each do |yard_object|
           if yard_object.type == :class
             tag = Swagger::Tag.new(yard_object)
           elsif tag && yard_object.type == :method
+
+            alias_name = Struct.new(:original, :alias)
+            yard_object.tags.each do |_tag|
+              alias_names << alias_name.new(createOriginalPath(yard_object, tag), _tag.text) if _tag.tag_name == 'name'
+            end
+            
             operation = Swagger::Operation.new(yard_object, tag, routes)
             operations << operation if operation.valid?
           end
@@ -20,9 +27,16 @@ module Swaggard
 
         return unless operations.any?
 
-        return tag, operations
+        return tag, operations, alias_names
+      end
+
+      private
+
+      def createOriginalPath(yard_object, tag)
+        "#{tag.controller_class.to_s}.#{yard_object.name}_body"
       end
 
     end
   end
 end
+
